5453263a75ee6111fa38f24152827549fb9326b3
iantaiahn <iantaiahn@yahoo.com>
2022-08-20 17:18:41 -0600

    Figured out the camera work to follow the player.

--- phaser/js/phaser.js
+++ phaser/js/phaser.js
@@ -41,13 +41,16 @@ function preload ()
 
 function create ()
 {
+    this.cameras.main.setBounds(0,0, 3200, 1800);
+    this.physics.world.setBounds(0,0, 1600, 900);
+
     //  A simple background for our game
     var background = this.add.image(800, 100, 'sky');
     background.setScale(2, 1);
 
     // green rectangle, will replace with actual ground, and assests, and such.
     var platforms = this.physics.add.staticGroup();
-    platforms.create(800, 870, 'ground').setScale(4, 30).refreshBody();
+    platforms.create(800, 870, 'ground').setScale(5, 30).refreshBody();
 
     // the actual colliders seperating the player from the sky, and the edge of the screen.
     var groundCollider = this.physics.add.staticGroup();
@@ -86,39 +89,29 @@ function create ()
     cursors = this.input.keyboard.createCursorKeys();
     this.physics.add.collider(player, groundCollider);
 
+    // Camera Work
+    // follows the player at the center.
+    this.cameras.main.startFollow(player);
+    // this.cameras.main.followOffset.set(200, 200);
+    this.cameras.main.setZoom(1.5);
 
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    // stars = this.physics.add.group({
-    //     key: 'star',
-    //     repeat: 11,
-    //     setXY: { x: 12, y: 0, stepX: 130 }
-    // });
-    
-    // stars.children.iterate(function (child) {
-    // });
 
-    // bombs = this.physics.add.group();
+    // interesting bit of code.. WIll study it more.
+    // I think this is used for generating mobs.
+    // for (var i=0; i < 10; i++)
+    // {
+    //     game.add.sprite(game.world.randomX, game.world.randomY, 'baddie');
+    // }
 
-    //  The score
-    // scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Collide the player and the stars with the platforms
-    // this.physics.add.collider(bombs, groundCollider);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    // this.physics.add.overlap(player, stars, collectStar, null, this);
-    // this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update ()
 {
-    // if (gameOver)
-    // {
-    //     return;
-    // }
-
     // TODO Bug that doesn't allow player to move right if left button is pressed down.
+    // Bug also is prevalent in their example I found out, so this just may be how it is.
+    // Probably can clean up if statements to be more readable as well.
     // moves player left
+    // const cam = this.cameras.main;
     if (cursors.left.isDown)    {
         player.setVelocityX(-200);
         player.anims.play('left', true);
@@ -154,43 +147,4 @@ function update ()
     }
 }
 
-/*
-function collectStar (player, star)
-{
-    star.disableBody(true, true);
 
-    //  Add and update the score
-    score += 10;
-    scoreText.setText('Score: ' + score);
-
-    if (stars.countActive(true) === 0)
-    {
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
-            child.enableBody(true, child.x, 0, true, true);
-        });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
-
-    }
-}
-*/
-
-/*
-function hitBomb (player, bomb)
-{
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    gameOver = true;
-}
-*/
